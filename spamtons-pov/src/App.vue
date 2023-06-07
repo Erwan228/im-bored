4af2d34b58af51c35d7675e521f0d22cfddf3e58
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-07 12:52:48 +0200

    d

--- spamtons-pov/src/App.vue
+++ spamtons-pov/src/App.vue
@@ -2,16 +2,42 @@
   <div id="divflex">
     <div id="pink" class="eyes"></div><div id="yello" class="eyes"></div>
   </div>
-  <p>HEY EVERY !! IT'S ME!!! EV3RY BUDDY 'S FAVORITE [[Number 1 Rated Salesman1997]] SPAMT SPAMTON G. SPAMTON!!</p>
-
+  <p @click="trappedClick">HEY EVERY !! IT'S ME!!! EV3RY BUDDY 'S FAVORITE [[Number 1 Rated Salesman1997]] SPAMT SPAMTON G. SPAMTON!!</p>
+  <salesPitch v-if="salesPitchShow" @click="showDealLMA0">
+    <template v-slot:salesPitch1>
+      <p>DO YOU WANNA HEAR ABOUT THE [[0.99$ Deal]] YOU CAN MAKE WITH ME?<br>
+      BECAUSE I KNOW. EXCACTLY. WHAT YOU. WANT!!!!1!</p>
+    </template>
+  </salesPitch>
+  <br>
+  <salesPitch v-if="showPitch2" @click="showTheProduct$$$$">
+    <template v-slot:salesPitch2>
+      <p>AND. THAT. IS ..</p>
+    </template>
+  </salesPitch>
+  
 </template>
 
 <script>
 import salesPitch from './components/salesPitch.vue';
 export default {
   name: 'App',
+  data() {
+    return {
+      salesPitchShow: false,
+      showPitch2: false,
+    }
+  },
   components: {
     salesPitch,
+  },
+  methods: {
+    trappedClick(){
+      this.salesPitchShow = true
+    },
+    showDealLMA0(){
+      this.showPitch2 = true
+    }
   }
 
 }
--- spamtons-pov/src/components/salesPitch.vue
+++ spamtons-pov/src/components/salesPitch.vue
@@ -1,7 +1,4 @@
-<template>
-    <p>DO YOU WANNA HEAR ABOUT THE [[0.99$ Deal]] YOU CAN MAKE WITH ME?<br>
-    BECAUSE I KNOW. EXCACTLY. WHAT YOU. WANT!!!!1!</p>
-</template>
+
 
 <script>
 export default {
