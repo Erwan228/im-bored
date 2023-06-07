208b4fd503078d40d8b7311f952d89752fdcfeee
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-07 11:23:05 +0200

    Update salesPitch.vue

---
+++ spamtons-pov/src/components/salesPitch.vue
@@ -0,0 +1,10 @@
+<template>
+    <p>DO YOU WANNA HEAR ABOUT THE [[0.99$ Deal]] YOU CAN MAKE WITH ME?<br>
+    BECAUSE I KNOW. EXCACTLY. WHAT YOU. WANT!!!!1!</p>
+</template>
+
+<script>
+export default {
+    name: "salesPitch",
+}
+</script>
