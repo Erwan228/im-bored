5d4ad2626f3a333e74e377348165aa4b43268133
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-07 10:44:41 +0200

    Update App.vue

--- spamtons-pov/src/App.vue
+++ spamtons-pov/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>cum</p>
+  <div id="divflex">
+    <div id="pink" class="eyes"></div><div id="yello" class="eyes"></div>
+  </div>
 </template>
 
 <script>
@@ -18,5 +20,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#divflex{
+  display:flex;
+  justify-content: space-evenly;
+}
+.eyes{
+  height: 404px;
+  width: 404px;
+}
+#pink{
+  background-color: pink;
+}
+#yello{
+  background-color: yellow;
 }
 </style>
